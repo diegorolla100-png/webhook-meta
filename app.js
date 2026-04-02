4c8cd861aa33329b13ebbbec65d41196610df7e4
diegorolla100-png <diegorolla100@gmail.com>
2026-04-02 02:23:26 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const challenge = req.query['hub.challenge'];
+  const token = req.query['hub.verify_token'];
+
+  if (mode === 'subscribe' && token === 'mdcu_verify_123') {
+    return res.status(200).send(challenge);
+  } else {
+    return res.sendStatus(403);
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server running');
+});
